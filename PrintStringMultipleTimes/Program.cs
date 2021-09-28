26d00d9f201c2b0052bf66c45f3ef621aac0428a
Asish <mohapatra.asish@gmail.com>
2021-09-28 10:46:22 +0800

    PrintString MultipleTimes using Enumerable.Repeat

---
+++ PrintStringMultipleTimes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrintStringMultipleTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Print("Asish", 5));
+        }
+
+        static string Print(string str, int N)
+        {
+            return string.Concat(Enumerable.Repeat(str, N));
+        }
+    }
+}
