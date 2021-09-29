21169e3c18b31d737850d0f563ddb0642abdd2e1
Asish <mohapatra.asish@gmail.com>
2021-09-29 09:03:31 +0800

    Prime Number

--- PrintStringMultipleTimes/Program.cs
+++ PrintStringMultipleTimes/Program.cs
@@ -10,14 +10,34 @@ namespace PrintStringMultipleTimes
         static void Main(string[] args)
         {
             Console.WriteLine(PrintItems<int>.ConcatMultiple(12, 5));
+            while (int.TryParse(Console.ReadLine(), out int number))
+            {
+                Console.WriteLine($"Number {number} is {(Number.IsPrime(number)? "PRIME" : "COMPOSITE")}");
+            }
         }
     }
-            
-    public class PrintItems<T> where T :  new()
+
+    public class PrintItems<T> where T : new()
     {
         public static string ConcatMultiple(T item, int N)
         {
             return string.Concat(Enumerable.Repeat(item.ToString() + " ", N));
+        }
+    }
+
+    public class Number
+    {
+        public static bool IsPrime(int n)
+        {
+            bool isPrime = true;
+            for (int i = 2; i <= n / 2; i++)
+            {
+                if (n % i == 0)
+                {
+                    isPrime = false;
+                }
+            }
+            return isPrime;
         }
     }
 }
